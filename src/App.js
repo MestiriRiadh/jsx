d8904b3ec774be923caececb526b39347c337c31
MestiriRiadh <mestiri.riadh@gmail.com>
2021-03-24 22:15:53 +0100

    jsx

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+import umbrella from "./umbrellaSrc.jpg";
+function App() {
+  return (
+    <div>
+      <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+        <h1 className="title red">Your name here</h1>
+
+        <br />
+
+        <img src="/UmbrellaInPublic.jpg" width="500" />
+
+        <br />
+
+        <img src={umbrella} />
+      </div>
+
+      <video width="320" height="240" controls>
+        <source src="/2020.mp4" type="video/mp4" />
+      </video>
+    </div>
+  );
+}
+
+export default App;
